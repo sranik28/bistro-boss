0a3185548dc39aeaa597560fe7b51d7fd0807c2f
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-05-24 06:47:39 +0600

    fooder added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaFacebookF, FaInstagram, FaTwitter  } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='grid md:grid-cols-2 text-white'>
+                <div className='bg-[#1F2937] text-center py-16'>
+                    <h2 className='text-2xl my-3'>CONTACT US</h2>
+                    <p>123 ABS Street, Uni 21, Bangladesh</p>
+                    <p>+88 123456789</p>
+                    <p>Mon - Fri: 08:00 - 22:00</p>
+                    <p>Sat - Sun: 10:00 - 23:00</p>
+                </div>
+                <div className='bg-[#111827]  py-16  text-center  '>
+                    <h2 className='text-xl my-4'>Follow US</h2>
+                    <p>Join us on social media</p>
+                    <div className='flex gap-5 justify-center mt-5'>
+                        <span><FaFacebookF className='w-5 h-5' /></span>
+                        <span><FaInstagram className='w-5 h-5' /></span>
+                        <span><FaTwitter className='w-5 h-5' /></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../pages/header/NavBar';
+import Footer from '../components/Footer';
 
 const Root = () => {
     return (
         <div className=''>
             <NavBar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
